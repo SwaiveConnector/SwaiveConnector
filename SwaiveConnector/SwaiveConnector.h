c703276f4e9b1dd291bb71fa15d992b617d69eb2
krishnaraj1601 <krishnaraj@kliotech.com>
2014-12-04 16:22:10 +0530

    Initial Commit

---
+++ SwaiveConnector/SwaiveConnector.h
@@ -0,0 +1,13 @@
+//
+//  SwaiveConnector.h
+//  SwaiveConnector
+//
+//  Created by krishnaraj on 04/12/14.
+//  Copyright (c) 2014 kliotech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwaiveConnector : NSObject
+
+@end
---
+++ SwaiveConnector/SwaiveConnector.m
@@ -0,0 +1,13 @@
+//
+//  SwaiveConnector.m
+//  SwaiveConnector
+//
+//  Created by krishnaraj on 04/12/14.
+//  Copyright (c) 2014 kliotech. All rights reserved.
+//
+
+#import "SwaiveConnector.h"
+
+@implementation SwaiveConnector
+
+@end
